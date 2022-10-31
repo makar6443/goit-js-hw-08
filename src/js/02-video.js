fe9f4221b22a4389d32555932c771ba2ad373bef
makar6443 <107273992+makar6443@users.noreply.github.com>
2022-10-31 20:53:25 +0200

    2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
 
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+const player = new Player(iframe);
+
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(event) {
+    localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
+};
+
+const getLocalStorageValue = localStorage.getItem(LOCALSTORAGE_KEY); 
+
+if (getLocalStorageValue === null || getLocalStorageValue === undefined) {
+    return ;
+} else {
+    player.setCurrentTime(getLocalStorageValue);
+};
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from "lodash.throttle";
+
+const form = document.querySelector('.feedback-form');
+
+const email = document.querySelector('input');
+
+const textarea = document.querySelector('textarea');
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(fillForm, 500));
+form.addEventListener('submit', submitForm);
+
+function fillForm(){
+    const newObjSave = {email: email.value, message: textarea.value };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(newObjSave));
+};
+
+function submitForm(evt) {
+    evt.preventDefault();
+    console.log({email: email.value, message: textarea.value });
+    form.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+};
+
+function loadingStorage () {
+    const getItemLOCALSTORAGE_KEY = localStorage.getItem(LOCALSTORAGE_KEY);
+    return getItemLOCALSTORAGE_KEY === null ? undefined : JSON.parse(getItemLOCALSTORAGE_KEY);
+};
+
+const storageData = loadingStorage();
+if (storageData) {
+  email.value = storageData.email;
+  textarea.value = storageData.message;
+};
+
+
+
+
+    
 
